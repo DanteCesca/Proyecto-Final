b7531f21158fca5335c6cfa5bb8a85842ab673e2
AnaSofiaCesca <sofiacesca.startek@gmail.com>
2026-06-22 15:03:36 -0300

    Resuelve conflicto App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
-import { useState } from 'react'
-import './Styles/App.css'
-import Especialidades from './Componentes/Especialidades'
-import PacienteInput from './Componentes/Pacientes'
+import { useState } from "react";
+import "./Styles/App.css";
+
+import Especialidades from "./Componentes/Especialidades";
+import Medicos from "./Componentes/Medicos";
+import PacienteInput from "./Componentes/Pacientes";
 
 function App() {
-  const [especialidad, setEspecialidad] = useState("")
-  const [nombrePaciente, setNombrePaciente] = useState("")
+  const [especialidad, setEspecialidad] = useState("");
+  const [nombrePaciente, setNombrePaciente] = useState("");
 
-return (
-  <div className="container mt-4">
-    <h1>Gestión de Turnos Médicos</h1>
+  return (
+    <div className="container mt-4">
+      <h1>Gestión de Turnos Médicos</h1>
 
-    <Especialidades
-      especialidadSeleccionada={especialidad}
-      onEspecialidadChange={setEspecialidad}
-    />
-    <PacienteInput
-     nombre={nombrePaciente}
-     onNombreChange={setNombrePaciente}
-    />
+      <Especialidades
+        especialidadSeleccionada={especialidad}
+        onEspecialidadChange={setEspecialidad}
+      />
 
-    <p>Especialidad: {especialidad}</p>
-    <p>Paciente: {nombrePaciente}</p>
-  </div>
-)
+      <PacienteInput
+        nombre={nombrePaciente}
+        onNombreChange={setNombrePaciente}
+      />
+
+      <Medicos
+        especialidadSeleccionada={especialidad}
+      />
+
+      <p>Especialidad: {especialidad}</p>
+      <p>Paciente: {nombrePaciente}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
