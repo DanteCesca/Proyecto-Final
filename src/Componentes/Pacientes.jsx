0aeca56f59bc2fa861fa07f732b383d85de563dc
AnaSofiaCesca <sofiacesca.startek@gmail.com>
2026-06-22 14:01:09 -0300

    Corrección de validaciones en input

--- src/Componentes/Pacientes.jsx
+++ src/Componentes/Pacientes.jsx
@@ -1,32 +1,43 @@
 import { useState } from "react";
 
+const MAX_CARACTERES = 30;
 const PacienteInput = ({ nombre, onNombreChange }) => {
   const [tocado, setTocado] = useState(false);
 
-  const hayError = tocado && !nombre.trim();
+  const hayError = tocado && nombre.length === 0;
+  const errorMinimo = tocado && nombre.length > 0 && nombre.length < 3;
+  const handleChange = (e) => {
+     onNombreChange(e.target.value); 
+  };
 
   return (
     <div className="mb-3">
       <label htmlFor="nombreApellido" className="form-label">
-        Nombre y Apellido
+        Nombre y Apellido 
       </label>
 
       <input
         type="text"
         id="nombreApellido"
-        className={`form-control ${hayError ? "is-invalid" : ""}`}
-        placeholder="Ej: Juan Pérez"
+        className={"form-control " + (hayError || errorMinimo ? "is-invalid" : "")}
+        placeholder="Ej:Pepe Honguito"
         value={nombre}
-        onChange={(e) => onNombreChange(e.target.value)}
+        onChange={handleChange}
         onBlur={() => setTocado(true)}
+        maxLength={MAX_CARACTERES}
         required
       />
 
       {hayError && (
         <div className="invalid-feedback">
-          El nombre y apellido es obligatorio.
+          El campo es obligatorio.
         </div>
       )}
+      {errorMinimo && (
+      <div className="invalid-feedback">
+        Debes ingresar al menos 3 caracteres.
+      </div>
+    )}
     </div>
   );
 };
