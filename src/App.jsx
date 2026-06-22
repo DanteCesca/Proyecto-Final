e8dc016307c07c86acdd6e3f7f04cc290aa43720
DanteCesca <dantecesca597@gmail.com>
2026-06-22 19:01:48 -0300

    Se corrigio el conflicto del app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import "./Styles/App.css"; 
-
 import Especialidades from "./Componentes/Especialidades";
 import Medicos from "./Componentes/Medicos";
 import PacienteInput from "./Componentes/Pacientes";
@@ -8,11 +7,6 @@ import PacienteInput from "./Componentes/Pacientes";
 function App() {
   const [especialidad, setEspecialidad] = useState("");
   const [nombrePaciente, setNombrePaciente] = useState("");
-import Especialidades from "./Componentes/Especialidades";
-import Medicos from "./Componentes/Medicos";
-
-function App() {
-  const [especialidad, setEspecialidad] = useState("");
 
   return (
     <div className="container mt-4">
@@ -23,20 +17,17 @@ function App() {
         onEspecialidadChange={setEspecialidad}
       />
 
-      <PacienteInput
-        nombre={nombrePaciente}
-        onNombreChange={setNombrePaciente}
-      />
-
       <Medicos
         especialidadSeleccionada={especialidad}
       />
 
-      <p>Especialidad: {especialidad}</p>
-      <p>Paciente: {nombrePaciente}</p>
-      <p>ID seleccionado: {especialidad}</p>
+      <PacienteInput
+        nombre={nombrePaciente}
+        onNombreChange={setNombrePaciente}
+      />
     </div>
   );
 }
-}
+
+
 export default App;
